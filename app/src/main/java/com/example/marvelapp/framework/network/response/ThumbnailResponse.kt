17389b7ea61cef440809e90a31fc547e96a23f23
Rogerio Lacorte Dias <lacortedias@gmail.com>
2023-02-01 09:14:54 -0300

    Revert "Revert "otimiza aplicativo de release""
    This reverts commit 60d8b79129fd20a3183bcf8103d44f6b119ab1cf.

--- app/src/main/java/com/example/marvelapp/framework/network/response/ThumbnailResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/ThumbnailResponse.kt
@@ -1,8 +1,12 @@
 package com.example.marvelapp.framework.network.response
 
+import com.google.gson.annotations.SerializedName
+
 data class ThumbnailResponse(
 
+    @SerializedName("path")
     val path: String,
+    @SerializedName("extension")
     val extension: String
 
 )
